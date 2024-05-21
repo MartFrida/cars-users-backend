bde947c77b1092e24e3e69cbe9215e0165cb784a
Mary <32392607+MartFrida@users.noreply.github.com>
2024-05-21 20:37:55 +0200

    auth signin user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,12 @@
-import * as authServices from "../services/authServices.js";
-import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import * as authServices from "../services/authServices.js"
+import 'dotenv/config'
+import ctrlWrapper from "../decorators/ctrlWrapper.js"
 import HttpError from '../helpers/HttpError.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const { JWT_SECRET } = process.env
+console.log(JWT_SECRET)
 
 const signup = async (req, res) => {
   const { email } = req.body
@@ -17,6 +23,29 @@ const signup = async (req, res) => {
   })
 }
 
+const signin = async (req, res) => {
+  const { email, password } = req.body
+  const user = await authServices.findUser({ email })
+  if (!user) {
+    throw HttpError(401, 'Email or password invalid') //Email invalid
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password)
+  if (!passwordCompare) {
+    throw HttpError(401, 'Email or password invalid') //Password invalid
+  }
+
+  const payload = {
+    id: user._id
+  }
+
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '23h' })
+
+  res.json({
+    token,
+  })
+}
+
 export default {
-  signup: ctrlWrapper(signup)
+  signup: ctrlWrapper(signup),
+  signin: ctrlWrapper(signin)
 }
--- services/authServices.js
+++ services/authServices.js
@@ -1,5 +1,11 @@
 import User from '../models/User.js'
+import bcrypt from 'bcrypt'
 
 export const findUser = async (filter) => User.findOne(filter)
 
-export const signup = async (data) => User.create(data)
+export const signup = async (data) => {
+  const { password } = data
+  const hashPassword = await bcrypt.hash(password, 10)
+
+  return User.create({ ...data, password: hashPassword })
+}
